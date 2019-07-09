8074907a594b83d856785450147052de9ea15960
Bartosz Stajnowski <bstajnowski@it-dev.pl>
2019-07-09 15:22:55 +0200

    Npm package installed

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const validator = require('validator');
+
+
+
+
+
 const fs = require('fs');
+const sum = require("./utils.js")
+
+console.log(validator.isEmail('andrew@exampl.com'));
 
 fs.writeFileSync("notes.txt","???????????????????");
